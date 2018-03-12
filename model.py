3d9d849c8b9b337b419a2d4aed30f48ff3b3d76a
Julius Visto <agentvis2@gmail.com>
2018-03-12 13:00:14 +0800

    finished the structure of model

--- model.py
+++ model.py
@@ -1,14 +1,49 @@
 from index import db
 from datetime import datetime
 
+class Incubator(db.Model):
+    __tablename__ = 'incubators'
+    id = db.Column(db.Integer, primary_key = True)
+    tray_id= db.relationship('Tray', backref='incubator')
+    name = db.Column(db.String(30), unique=True, index=True, default='Incubator '+str(id))
+    rows = db.Column(db.Integer, nullable=False)
+    column = db.Column(db.Integer, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    balot = db.Column(db.Integer, default = 0)
+    freska = db.Column(db.Integer, default = 0)
+    matra = db.Column(db.Integer, default = 0)
+    echo = db.Column(db.Integer, default = 0)
+    penoy = db.Column(db.Integer, default = 0)
+
+
+    def __repr__(self):
+        return '<Role %r>' %self.name
 
 class Tray(db.Model):
     __tablename__ = 'trays'
     id = db.Column(db.Integer, primary_key=True)
-    date_incubated = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    eggs_contained = db.Column(db.Integer)
+    date_incubated = db.Column(db.DateTime, nullable=False)
+    eggs_contained = db.Column(db.Integer, nullable=False, default=0)
+    incubator_id = db.Column(db.Integer, db.ForeignKey('incubators.id'))
 
     def __repr__(self):
         return '<Role %r>' %self.id
 
+class Daily_Harvest(db.Model):
+    __tablename__ = 'harvests'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
+    balot = db.Column(db.Integer, default = 0)
+    freska = db.Column(db.Integer, default = 0)
+    matra = db.Column(db.Integer, default = 0)
+    echo = db.Column(db.Integer, default = 0)
+    penoy = db.Column(db.Integer, default = 0)
+
+    def __repr__(self):
+        return '<Role %r>' %self.name
+
 db.create_all()
+
+entry1 = Incubator(rows=4,column=4,name='incubator chu')
+db.session.add(entry1)
+db.session.commit()
